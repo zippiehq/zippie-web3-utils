87d7c6b0fde88ec39b743fd0fef714d82b21c89a
Patrik Åkerlund <akerlund.patrik@gmail.com>
2022-05-23 21:26:32 +0200

    Use ethers.js to convert pubkey to address (instead of ethereumjs-util)

--- src/utility.js
+++ src/utility.js
@@ -1,25 +1,7 @@
-const ethutil = require('ethereumjs-util')
-
-const multisigContracts = {
-  kovan : {
-    '0x1FA9386D9B36aB866fe9d8fA958a41B83413B99B': {
-      creationBlock: 0,
-      version: 'v1'
-    },
-    '0xEEc94D4d1071fBab9D08cAd01a854301DBbf0C5B': {
-      creationBlock: 0,
-      version: 'v2'
-    } 
-  },
-  goerli: {
-
-  }
-}
+const ethers = require('ethers')
 
 function getEthAddressFromPubkey(pubkey) {
-  const zippieCardAddressBuffer = ethutil.pubToAddress(Buffer.from(pubkey.slice(2), 'hex'))
-  const zippieCardAddress = '0x' + Buffer.from(zippieCardAddressBuffer).toString('hex')
-  return zippieCardAddress
+  return ethers.utils.computeAddress("0x" + pubkey)
 }
 
 function getEthAddressFromTransactionTopic(topic) {
